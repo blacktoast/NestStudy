378bd42432894d220b3beb6fe61b31fe2e4596e0
blacktoast <dbstjd100@naver.com>
2021-11-30 23:00:04 +0900

    feat: 익스프레스 기본강의 공부중

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello world!!");
+import express from "express";
+const app = express();
+const port: number = 8000;
+
+app.get("/api/test", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({
+    name: "eom sun seong",
+    age: 45,
+  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
