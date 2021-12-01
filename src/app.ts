154e7aeb24d4b25143ec53f856bc717a4ac71eab
blacktoast <dbstjd100@naver.com>
2021-12-01 22:47:35 +0900

    TIL 21.12.1 -미들웨어 공부 -포스트맨 테스트하는법

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,36 @@
+import { Cat, CatType } from "./app.model";
 import express from "express";
 const app = express();
 const port: number = 8000;
+const data = [1, 2, 3, 4];
 
-app.get("/api/test", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send({
-    name: "eom sun seong",
-    age: 45,
-  });
+let myLogger = function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.log("LOGGED");
+  next();
+};
+let myLogger2 = function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.log("LOGGED2");
+  next();
+};
+app.use(myLogger);
+app.get("/", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
+
+app.use(myLogger2);
+
+app.post("/", (req, res) => {
+  res.send("jel");
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("server is on");
 });
