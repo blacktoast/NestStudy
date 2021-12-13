51367a3802f1fdf9d28574e3af511e5c7aeac270
blacktoast <dbstjd100@naver.com>
2021-12-13 22:56:49 +0900

    211.12.13 미들웨어 공부
    - use.app(함수) 로 미들웨어 사용
    -특정 라우터에만 적용하고싶으면
    use.get("경로",함수)형태로 작성
    next()함수로 미들웨어 실행후 라우팅하는 함수로 실행

--- src/app.ts
+++ src/app.ts
@@ -4,33 +4,34 @@ const app = express();
 const port: number = 8000;
 const data = [1, 2, 3, 4];
 
+function errorChecking(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  res.send({ error: "에러발생" });
+}
+
 let myLogger = function (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) {
-  console.log("LOGGED");
+  console.log(req.rawHeaders[1]);
   next();
 };
-let myLogger2 = function (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) {
-  console.log("LOGGED2");
-  next();
-};
+
 app.use(myLogger);
 app.get("/", (req, res) => {
-  console.log(req.rawHeaders[1]);
   res.send({ cats: Cat });
 });
-
-app.use(myLogger2);
-
-app.post("/", (req, res) => {
-  res.send("jel");
+app.get("/cats/blue", (req, res) => {
+  res.send({ blue: Cat[0] });
 });
+app.get("/cats/cute", (req, res) => {});
+
+app.use(errorChecking);
+
 app.listen(port, () => {
   console.log("server is on");
 });
